0a6523fa567244a7da93e68d7bb89fab01c67d78
Beka Sturua <beqasturua3@gmail.com>
2022-08-07 14:35:23 +0400

    Adding a Layout Wrapper Component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import AllQoutes from "./pages/AllQoutes";
 import QouteDetail from "./pages/QouteDetail";
 import NewQoute from "./pages/NewQoute";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQoutes />
-      </Route>
-      <Route path="/quotes/:qouteId">
-        <QouteDetail />
-      </Route>
-      <Route path="/new-qoute">
-        <NewQoute />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQoutes />
+        </Route>
+        <Route path="/quotes/:qouteId">
+          <QouteDetail />
+        </Route>
+        <Route path="/new-qoute">
+          <NewQoute />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 

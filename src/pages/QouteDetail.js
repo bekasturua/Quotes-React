659f079c4d29054eedaf4f6a892a90da62d486aa
Beka Sturua <beqasturua3@gmail.com>
2022-08-07 11:31:40 +0400

    useParams

--- src/pages/QouteDetail.js
+++ src/pages/QouteDetail.js
@@ -1,5 +1,15 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+
 const QouteDetail = () => {
-  return <h1>Qoute Detail Page</h1>;
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Qoute Detail Page</h1>
+      <p>{params.qouteId}</p>
+    </Fragment>
+  );
 };
 
 export default QouteDetail;

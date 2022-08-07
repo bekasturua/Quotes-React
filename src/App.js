c950dc7717e44b6c06087652caec1409ae113b8d
Beka Sturua <beqasturua3@gmail.com>
2022-08-07 11:31:17 +0400

    add Route and Redirecting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import AllQoutes from "./pages/AllQoutes";
+import QouteDetail from "./pages/QouteDetail";
+import NewQoute from "./pages/NewQoute";
+
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQoutes />
+      </Route>
+      <Route path="/quotes/:qouteId">
+        <QouteDetail />
+      </Route>
+      <Route path="/new-qoute">
+        <NewQoute />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
